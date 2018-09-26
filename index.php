2f9cd0e7407b408638dd548506b7ff1d609920de
David Persson <te16.david.persson@umea.nti.se>
2018-09-26 09:18:44 +0200

    första pushen

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="se">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">  
+	<title>Portalen till den hemligaste sidan</title>
+	<link href="https://fonts.googleapis.com/css?family=Quattrocento%7CQuattrocento+Sans" rel="stylesheet">
+	<style>
+	main {
+		width: 80%;
+		margin: 0px auto;
+	}
+	body {
+		font-family: 'Quattrocento', serif;
+	}
+	h1 {
+		font-family: 'Quattrocento Sans', sans-serif;
+	}
+	</style>
+</head>
+<body>
+<main>
+	<h1>Välkommen</h1>
+	<form action="login.php" method="POST">
+		<fieldset>
+			<legend>Logga in</legend>
+			<p>
+				<label for="username">Användarnamn: </label>
+				<input type="text" name="username" id="username">
+			</p>
+			<p>
+				<label for="password">Lösenord: </label>
+				<input type="password" name="password" id="password">
+			</p>
+			<p>
+				<input type="submit" name="submit" id="submit" value="Logga in">
+			</p>
+		</fieldset>
+	</form>
+</main>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">  
+	<title>Den hemliga sidan</title>
+</head>
+<body>
+<?php
+if(isset($_POST['submit'])) // if($_POST['submit'] == "Logga in")
+{
+	include 'db.php';
+
+	if(isset($_POST['username']) && isset($_POST['password']))
+	{
+		$username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+		$password = $_POST['password'];
+
+		$statement = $dbh->query("SELECT * FROM login");
+		$row = $statement->fetch(PDO::FETCH_ASSOC);
+
+		echo "<pre>" . print_r($row,1) . "</pre>";
+
+		if($username == $row['username']
+			&& password_verify($password, $row['password']))
+		{
+			echo "<h1>Välkommen in till den hemliga sidan.</h1>";
+		}
+		else
+		{
+			echo "<h1>Väldigt fel!</h1>";
+		}
+	}
+	// echo "<pre>" . print_r($_POST,1) . "</pre>";
+}
+else 
+{
+	echo "<h1>Vad gör du här?</h1>";
+}
+?>
+</body>
+</html>
